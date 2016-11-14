45f99a055de63829d8296a266206de9675a36c26
Bacto <adrien@bacto.net>
2016-11-14 16:55:31 +0100

    Force installation of modules to avoid bug on npm < 3

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ cp -fr boilerplate/. ../../
 mv ../../.gitignoreToCopy ../../.gitignore
 cd ../../
 npm i --save babel-watch
+npm i --save babel-plugin-transform-object-rest-spread
+npm i --save babel-polyfill
+npm i --save babel-preset-es2015

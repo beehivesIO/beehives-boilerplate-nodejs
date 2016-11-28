82ff69d38290d1c6032b7f5c8c0476068d2bf9a4
Bacto <adrien@bacto.net>
2016-11-28 16:36:56 +0100

    Resolve a bug on installation

--- install.sh
+++ install.sh
@@ -9,10 +9,12 @@ then
     babel-plugin-transform-object-rest-spread \
     babel-polyfill \
     babel-preset-es2015
+  yarn install
 else
   npm install --save \
     babel-watch \
     babel-plugin-transform-object-rest-spread \
     babel-polyfill \
     babel-preset-es2015
+  npm install
 fi

8eb696a9c45ee72dca46d4f3201a8f068d3d1ab0
Bacto <adrien@bacto.net>
2016-11-28 16:55:16 +0100

    Resolve a bug on start script

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ then
   yarn install
 fi
 
-node_modules/.bin/babel-watch --watch routes/ --presets es2015 --plugins transform-object-rest-spread node_modules/.bin/beehives-server-nodejs
+./node_modules/babel-watch/babel-watch.js --watch routes/ --presets es2015 --plugins transform-object-rest-spread node_modules/beehives-boilerplate-nodejs/build/server.js

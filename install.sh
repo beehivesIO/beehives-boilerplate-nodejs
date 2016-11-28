4514099e26c072297e1395594960392b16b0f30c
Bacto <adrien@bacto.net>
2016-11-28 16:17:17 +0100

    Remove Yarn from installation

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 cd ../../
-yarn add babel-watch babel-plugin-transform-object-rest-spread babel-polyfill babel-preset-es2015
+npm install --save \
+  babel-watch \
+  babel-plugin-transform-object-rest-spread \
+  babel-polyfill \
+  babel-preset-es2015

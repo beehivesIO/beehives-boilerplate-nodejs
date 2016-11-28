f589d7027c999fecfb7c710d54150d03210a4871
Bacto <adrien@bacto.net>
2016-11-28 16:30:20 +0100

    Use Yarn if possible during installation

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
 cd ../../
-npm install --save \
-  babel-watch \
-  babel-plugin-transform-object-rest-spread \
-  babel-polyfill \
-  babel-preset-es2015
+
+if [ -x "$(which yarn)" ]
+then
+  yarn add \
+    babel-watch \
+    babel-plugin-transform-object-rest-spread \
+    babel-polyfill \
+    babel-preset-es2015
+else
+  npm install --save \
+    babel-watch \
+    babel-plugin-transform-object-rest-spread \
+    babel-polyfill \
+    babel-preset-es2015
+fi

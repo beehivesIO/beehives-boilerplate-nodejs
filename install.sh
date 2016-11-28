90a04f35baea53cdec14a5601d53705f163c3797
Bacto <adrien@bacto.net>
2016-11-28 12:30:04 +0100

    Do not override files on reinstall package

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -fr boilerplate/. ../../
+cp -r boilerplate/. ../../
 mv ../../.gitignoreToCopy ../../.gitignore
 cd ../../
 yarn add babel-watch babel-plugin-transform-object-rest-spread babel-polyfill babel-preset-es2015
